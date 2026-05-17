d6f9af2b7f000a1f05c982e6715073e54adeb307
kenese <kenese.lautusi@gmail.com>
2026-05-18 10:17:22 +1200

    channel_4slot added

--- test/unit/engine_patch.test.js
+++ test/unit/engine_patch.test.js
@@ -25,3 +25,20 @@ test('channel_2slot abstraction routes two slots and one monitor path', () => {
     assert.equal((patch.match(/route \\\$1 \\\$2 monitor/g) || []).length, 1);
     assert.equal((patch.match(/line~/g) || []).length, 1);
 });
+
+test('channel_4slot abstraction routes four slots and one monitor path', () => {
+    const patchPath = path.join(__dirname, '..', '..', 'src', 'channel_4slot.pd');
+    assert.equal(fs.existsSync(patchPath), true);
+
+    const patch = fs.readFileSync(patchPath, 'utf8');
+
+    assert.match(patch, /route \\\$1 \\\$2 \\\$3 \\\$4 monitor/);
+    assert.match(patch, /looper_slot \\\$1/);
+    assert.match(patch, /looper_slot \\\$2/);
+    assert.match(patch, /looper_slot \\\$3/);
+    assert.match(patch, /looper_slot \\\$4/);
+    assert.match(patch, /line~/);
+    assert.equal((patch.match(/looper_slot/g) || []).length, 4);
+    assert.equal((patch.match(/route \\\$1 \\\$2 \\\$3 \\\$4 monitor/g) || []).length, 1);
+    assert.equal((patch.match(/line~/g) || []).length, 1);
+});
